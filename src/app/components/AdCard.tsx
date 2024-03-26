6f30fcacc3499197ce4cc9f4a0057b64d6fe1599
tarunsinghofficial <tarunsinghwap7@gmail.com>
2024-03-26 11:50:10 +0530

    fixed some mobile responsiveness issues

--- src/app/components/AdCard.tsx
+++ src/app/components/AdCard.tsx
@@ -10,15 +10,15 @@ interface AdCardProps {
 function AdCard({ backgroundImageUrl, title, description }: AdCardProps) {
     return (
         <div className='w-[10rem] h-[25rem]  md:w-[15rem] md:h-[32rem] xl:w-[238.53px] xl:h-[511px] bg-white dark:bg-[#3B3E47] drop-shadow-md hover:drop-shadow-xl p-2 flex flex-col gap-2'>
-            <div className='border-[0.2px] border-[#006555]'>
-            <div className='relative'>
-                <Image src={backgroundImageUrl} alt={title} width={1000} height={1000} />
-                <div className='bg-black dark:text-white w-auto h-auto py-1 px-5 absolute right-0 top-0 '>Ad</div>
-            </div>
-            <div className='flex flex-col gap-3 p-0 lg:p-4'>
-                <h2 className="text-md md:text-lg lg:text-xl font-medium text-left pl-1 text-[#222D3A] dark:text-white">{title}</h2>
-                <p className='text-[#525965] dark:text-[#DFDFDF] text-xs md:text-xs lg:text-sm overflow-hidden'>{description}</p>
-            </div>
+            <div className='border-[0.2px] border-[#006555] w-full h-full'>
+                <div className='relative'>
+                    <Image src={backgroundImageUrl} alt={title} width={1000} height={1000} />
+                    <div className='bg-black dark:text-white w-auto h-auto py-1 px-5 absolute right-0 top-0 '>Ad</div>
+                </div>
+                <div className='flex flex-col gap-3 p-0 lg:p-3'>
+                    <h2 className="text-md md:text-lg lg:text-xl font-medium text-left pl-1 text-[#222D3A] dark:text-white">{title}</h2>
+                    <p className='text-[#525965] dark:text-[#DFDFDF] text-xs md:text-xs lg:text-sm overflow-hidden ml-1'>{description}</p>
+                </div>
             </div>
         </div>
     )
--- src/app/components/SportsCard.tsx
+++ src/app/components/SportsCard.tsx
@@ -11,18 +11,18 @@ interface SportsCardProps {
 function SportsCard({ backgroundImageUrl, title, totalEvents, sport } : SportsCardProps) {
 
   return (
-    <div className='w-[10rem] h-[25rem]  md:w-[15rem] md:h-[32rem] xl:w-[238.53px] xl:h-[511px] bg-white dark:bg-[#3B3E47] drop-shadow-md hover:drop-shadow-xl p-2 flex flex-col justify-between space-y-2'>
+    <div className='w-[10rem] h-[25rem] md:w-[15rem] md:h-[32rem] xl:w-[238.53px] xl:h-[511px] bg-white dark:bg-[#3B3E47] drop-shadow-md hover:drop-shadow-xl p-2 flex flex-col justify-between space-y-2'>
         <Image src={backgroundImageUrl} alt={title} width={1000} height={1000} />
         <h2 className="text-md md:text-lg xl:text-xl font-medium text-left pl-1 dark:text-white">{title}</h2>
         <div className='flex flex-col items-center'>
-        <div className='bg-[#F7F7F8] dark:bg-[#292B32] w-full h-auto p-2 flex flex-row gap-4 justify-between items-center'>
+        <div className='bg-[#F7F7F8] dark:bg-[#292B32] w-full h-[3em] p-2 flex flex-row gap-4 justify-between items-center'>
             <div>
                 <p className='text-[10px] md:text-[12px] xl:text-[14px] text-[#525965] dark:text-[#DFDFDF]'>Total Events</p>
-                <p className='text-[12px] md:text-[14px] xl:text-[16px]  dark:text-white'>{totalEvents} Events</p>
+                <p className='text-[11px] md:text-[14px] xl:text-[16px]  dark:text-white'>{totalEvents} Events</p>
             </div>
             <div>
                 <p className='text-[10px] md:text-[12px] xl:text-[14px] text-[#525965] dark:text-[#DFDFDF]'>Sport</p>
-                <p className='text-[12px] md:text-[14px] xl:text-[16px] text-black dark:text-white'>{sport}</p>
+                <p className='text-[11px] md:text-[14px] xl:text-[16px] text-black dark:text-white'>{sport}</p>
             </div>
         </div>
         </div>
